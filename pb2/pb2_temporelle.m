fa6bdc98aeb1c595b8c81cb87b819f2af2e1a0b0
Thomas Chardonnens <61554870+ThomasCdnns@users.noreply.github.com>
2022-01-18 22:26:28 +0100

    updated pb2_temporelle

--- pb2/pb2_temporelle.m
+++ pb2/pb2_temporelle.m
@@ -1,33 +1,41 @@
 clc
 clear
-[y, Fe] = audioread('100.wav');
-t = linspace(0,length(y)/Fe,length(y));
-figure(1) 
+
+file="105.wav";
+disp(getBPM(file))
+
+function bpm = getBPM(file)
+
+[y, Fs] = audioread(file);
+Ts = 1/Fs;
+y_size = length(y);
+duree = (y_size-1)*Ts;
+t = 0:Ts:duree;
+figure(1)
 plot(t,y)
 
 R = zeros(1,10);
 
-A=y;
-maxVal = max(A(1:Fe));
-indinceMax = find(A == maxVal);
-c = indinceMax(indinceMax>=1 & indinceMax<=(Fe));
+max_value = max(y(1:Fs));
+max_indice = find(y == max_value);
+pic2 = max_indice(max_indice>=1 & max_indice<=Fs);
 
 k=1;
-for i = 1:1:2*length(y)/Fe-3
-maxVal1 = max(A(Fe +(Fe*i)/2 : (Fe*i)/2 + 3*Fe/2 ));
-    if maxVal1 <= maxVal*1.1 && maxVal1 >= maxVal*0.9
-        indincex1 = find(A == maxVal1);
-        b = indincex1(indincex1>=(Fe*i/2+Fe) & indincex1<=(Fe*i/2 + 3*Fe/2));
-        distance = b(1) - c(1);
-        if distance>=126
-            c = b;
-            tempsDiff=10*distance/3600;
-            bpm = 60/tempsDiff(1);
+for i = 1:1:2*length(y)/Fs-3
+max_value_1 = max(y(Fs+(Fs*i)/2:(Fs*i)/2+3*Fs/2));
+    if max_value_1 <= max_value*1.15 && max_value_1>=max_value*0.85
+        max_indice_1 = find(y == max_value_1);
+        pic1 = max_indice_1(max_indice_1>=(Fs*i/2+Fs) & max_indice_1<=(Fs*i/2 + 3*Fs/2));
+        distance = pic1(1) - pic2(1);
+        if distance>=50
+            pic2 = pic1;
+            deltaT=10*distance/3600;
+            bpm = 60/deltaT(1);
             R(k) = ceil(bpm);
             k=k+1;
         end
     end
 end
-disp(sum(R)/k+1 + " bpm")
-
+bpm=sum(R)/k+1;
+end
 
